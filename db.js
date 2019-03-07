4db1bc2582fd81e6da670c4848d0dae31622a517
sean chan <sean@seans-MacBook-Air.local>
2019-03-07 19:13:18 +0800

    Test

--- db.js
+++ db.js
@@ -8,12 +8,34 @@ const url = require('url');
  * ===================================
  */
 
-const configs = {
-  user: 'sean',
-  host: '127.0.0.1',
-  database: 'pokemon',
-  port: 5432,
-};
+ //check to see if we have this heroku environment variable
+if( process.env.DATABASE_URL ){
+
+  //we need to take apart the url so we can set the appropriate configs
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  //make the configs object
+  var configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+
+}else{
+
+  //otherwise we are on the local network
+  const configs = {
+    user: 'sean',
+    host: '127.0.0.1',
+    database: 'pokemon',
+    port: 5432,
+  };
+}
 
 const pool = new pg.Pool(configs);
 
